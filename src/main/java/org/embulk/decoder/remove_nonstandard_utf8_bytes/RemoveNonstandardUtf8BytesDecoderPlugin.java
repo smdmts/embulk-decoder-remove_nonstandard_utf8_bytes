e93733c83cfe376d4fbf77eb7279a309cf02dee0
masatoshi.shimada <masatoshi.shimada@every.tv>
2017-06-02 11:05:14 +0900

    remove unused import.

--- src/main/java/org/embulk/decoder/remove_nonstandard_utf8_bytes/RemoveNonstandardUtf8BytesDecoderPlugin.java
+++ src/main/java/org/embulk/decoder/remove_nonstandard_utf8_bytes/RemoveNonstandardUtf8BytesDecoderPlugin.java
@@ -1,7 +1,5 @@
 package org.embulk.decoder.remove_nonstandard_utf8_bytes;
 
-import com.google.common.base.Charsets;
-import com.google.common.io.CharStreams;
 import org.embulk.config.ConfigInject;
 import org.embulk.config.ConfigSource;
 import org.embulk.config.Task;
@@ -12,15 +10,8 @@ import org.embulk.spi.FileInput;
 import org.embulk.spi.util.FileInputInputStream;
 import org.embulk.spi.util.InputStreamFileInput;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.nio.charset.CharsetDecoder;
-import java.nio.charset.CodingErrorAction;
-import java.nio.charset.StandardCharsets;
 
 public class RemoveNonstandardUtf8BytesDecoderPlugin
         implements DecoderPlugin
